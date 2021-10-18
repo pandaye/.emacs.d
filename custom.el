f44ec1b25dd7dc6e06286636b970263afde790ee
panjunxun <jxpan@stu.xidian.edu.cn>
2021-10-18 16:26:52 +0800

    LaTeX 添加 lsp 支持，使用 company box，调整 lsp-headerline 样式

--- custom.el
+++ custom.el
@@ -3,12 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-auto-local ".auto")
+ '(TeX-auto-local ".ltxauto")
  '(delete-selection-mode nil)
- '(org-agenda-files
-   '("~/.emacs.d/myinit.org" "/home/pandaye0/.org-gtd/inbox.org" "/home/pandaye0/.org-gtd/project.org" "/home/pandaye0/.org-gtd/note.org" "/home/pandaye0/.org-gtd/task.org" "/home/pandaye0/.org-gtd/trash.org" "/home/pandaye0/.org-gtd/finished.org"))
  '(package-selected-packages
-   '(projectile helm-lsp org-faces org-fragtog org-appear org-superstar valign diff-hl lsp-python-ms yasnippet htmlize yaml-mode wanderlist racket-mode jedi wanderlust helm-xref ccls lsp cnfonts lsp-imenu lsp-python company-jedi emacs-cquery lsp-ui doom-themes cquery lsp-mode nlinum magit auctex yasnippet-snippets ox-gfm markdown-mode flycheck-plantuml plantuml-mode graphviz-dot-mode cmake-mode flycheck-rtags flycheck-irony company-irony-c-headers company-irony irony company-rtags rtags paredit helm-swoop helm elpy flycheck spacemacs-theme company ace-window which-key use-package try smooth-scrolling rainbow-delimiters powerline org-bullets ob-ipython neotree hlinum all-the-icons)))
+   '(company-box projectile helm-lsp org-faces org-fragtog org-appear org-superstar valign diff-hl lsp-python-ms yasnippet htmlize yaml-mode wanderlist racket-mode jedi wanderlust helm-xref ccls lsp cnfonts lsp-imenu lsp-python company-jedi emacs-cquery lsp-ui doom-themes cquery lsp-mode nlinum magit auctex yasnippet-snippets ox-gfm markdown-mode flycheck-plantuml plantuml-mode graphviz-dot-mode cmake-mode flycheck-rtags flycheck-irony company-irony-c-headers company-irony irony company-rtags rtags paredit helm-swoop helm elpy flycheck spacemacs-theme company ace-window which-key use-package try smooth-scrolling rainbow-delimiters powerline org-bullets ob-ipython neotree hlinum all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,5 +20,13 @@
  '(flycheck-warning ((t (:underline "#FCCE7B"))))
  '(flymake-error ((t (:underline "#ff665c"))))
  '(flymake-warning ((t (:underline nil))))
+ '(lsp-headerline-breadcrumb-path-error-face ((t (:inherit lsp-headerline-breadcrumb-path-face :underline "Red1"))))
+ '(lsp-headerline-breadcrumb-path-hint-face ((t (:inherit lsp-headerline-breadcrumb-path-face :underline "Green"))))
+ '(lsp-headerline-breadcrumb-path-info-face ((t (:inherit lsp-headerline-breadcrumb-path-face :underline "Green"))))
+ '(lsp-headerline-breadcrumb-path-warning-face ((t (:inherit lsp-headerline-breadcrumb-path-face :underline "Yellow"))))
+ '(lsp-headerline-breadcrumb-symbols-error-face ((t (:inherit lsp-headerline-breadcrumb-symbols-face :underline "Red1"))))
+ '(lsp-headerline-breadcrumb-symbols-hint-face ((t (:inherit lsp-headerline-breadcrumb-symbols-face :underline "Green"))))
+ '(lsp-headerline-breadcrumb-symbols-info-face ((t (:inherit lsp-headerline-breadcrumb-symbols-face :underline "Green"))))
+ '(lsp-headerline-breadcrumb-symbols-warning-face ((t (:inherit lsp-headerline-breadcrumb-symbols-face :underline "Yellow"))))
  '(lsp-ui-sideline-current-symbol ((t (:foreground "black" :box (:line-width -1 :color "gray") :weight ultra-bold :height 0.99))))
  '(wl-highlight-summary-displaying-face ((t (:underline nil :weight ultra-bold)))))
