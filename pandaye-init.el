64658e251904b975d53aab784cf31b3df8cf76b7
panjunxun <panjunxun@develop-lxc.local>
2026-08-12 15:51:12 +0800

    fix(markdown): 忽略单词内下划线强调

--- pandaye-init.el
+++ pandaye-init.el
@@ -382,6 +382,28 @@
   (markdown-fontify-code-blocks-natively t)
   (markdown-fontify-code-block-default-mode 'fundamental-mode)
   :config
+  (defun my-markdown-match-italic-skip-intraword-underscore (orig-fun last)
+    "Make `markdown-mode' ignore intraword underscore emphasis like GFM."
+    (let (found done)
+      (while (and (not done) (funcall orig-fun last))
+        (if (and (not (derived-mode-p 'gfm-mode))
+                 (eq (char-after (match-beginning 0)) ?_)
+                 (not (markdown--gfm-markup-underscore-p
+                       (match-beginning 0)
+                       (match-end 3))))
+            (progn
+              (goto-char (min (1+ (match-beginning 0)) last))
+              (unless (< (point) last)
+                (setq done t)))
+          (setq found t
+                done t)))
+      found))
+
+  (unless (advice-member-p #'my-markdown-match-italic-skip-intraword-underscore
+                           'markdown-match-italic)
+    (advice-add 'markdown-match-italic
+                :around #'my-markdown-match-italic-skip-intraword-underscore))
+
   (set-face-attribute 'markdown-code-face nil
                       :background "#32302f"
                       :extend t)
